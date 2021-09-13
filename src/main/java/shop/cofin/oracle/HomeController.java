fa4474b170d8e39d8c06451d51bf7829cdb9d7e8
YoonHyunSung <gustjd6495@gmail.com>
2021-09-13 15:09:26 +0900

    0913

--- src/main/java/shop/cofin/oracle/HomeController.java
+++ src/main/java/shop/cofin/oracle/HomeController.java
@@ -22,18 +22,15 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping(value = "/joinform", method = RequestMethod.GET)
+	public String join(Locale locale, Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "index";
+		return "/user/join";
+	}
+	@RequestMapping(value = "/loginform", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+				
+		return "/user/login";
 	}
 	
 }
